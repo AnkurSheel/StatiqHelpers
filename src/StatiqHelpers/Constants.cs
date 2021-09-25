2cc0a343a4f77381c0236ffef0a006cdbe3cb87a
Ankur Sheel <ankur.sheel@gmail.com>
2021-09-26 01:59:49 +1200

    add constants file

---
+++ src/StatiqHelpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace StatiqHelpers
+{
+    public class Constants
+    {
+        public const string AssetsDirectory = "assets";
+        public static readonly string ImagesDirectory = $"{AssetsDirectory}/images";
+        public static readonly string JsDirectory = $"{AssetsDirectory}/js";
+        public static readonly string PostImagesDirectory = $"{ImagesDirectory}/posts";
+        public static readonly string SocialImagesDirectory = $"{ImagesDirectory}/social";
+        public static readonly string FontsDirectory = $"{AssetsDirectory}/fonts";
+        public static readonly string BlogPath = $"blog";
+    }
+}
