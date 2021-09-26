c163683cc95b89fcf31cd66311a091a960b1794d
Ankur Sheel <ankur.sheel@gmail.com>
2021-09-26 17:27:10 +1300

    add rss module

---
+++ src/StatiqHelpers/Rss/GenerateRssMetaData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Statiq.Common;
+using Statiq.Feeds;
+
+namespace StatiqHelpers.Rss
+{
+    public class GenerateRssMetaData : ParallelModule
+    {
+        protected override Task<IEnumerable<IDocument>> ExecuteInputAsync(IDocument input, IExecutionContext context)
+        {
+            context.LogDebug($"Read file {input.Source}");
+
+            return Task.FromResult(
+                input.Clone(
+                        new MetadataItems
+                        {
+                            { FeedKeys.Description, input.GetExcerpt() },
+                            { FeedKeys.Published, input.GetPublishedDate() },
+                            { FeedKeys.Image, input.GetCoverImageLink() }
+                        })
+                    .Yield());
+        }
+    }
+}
