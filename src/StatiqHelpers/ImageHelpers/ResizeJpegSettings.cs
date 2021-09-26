340d2173e96abcd08b109de618df10885e71755e
Ankur Sheel <ankur.sheel@gmail.com>
2021-09-26 18:46:58 +1300

    add resize jpeg command

---
+++ src/StatiqHelpers/ImageHelpers/ResizeJpegSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using Spectre.Console.Cli;
+using Statiq.App;
+
+namespace StatiqHelpers.ImageHelpers
+{
+    public class ResizeJpegSettings : EngineCommandSettings
+    {
+        [CommandArgument(0, "<width>")]
+        [Description("Specify the target width")]
+        public int Width { get; set; }
+
+        [CommandArgument(0, "<height>")]
+        [Description("Specify the target height")]
+        public int Height { get; set; }
+
+        [CommandOption("-c")]
+        [Description("Compress all files, not just uncommitted files")]
+        public bool AllFiles { get; set; }
+    }
+}
