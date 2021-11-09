9a22f8b30dd5b387e1e255b3516be1d8c8232b2b
Ankur Sheel <ankur.sheel@gmail.com>
2021-11-10 12:44:52 +1300

    feat: add shortcode for responsive Youtube videos

---
+++ src/StatiqHelpers/Shortcodes/ResponsiveYouTubeShortcode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using Statiq.Common;
+
+namespace StatiqHelpers.Shortcodes
+{
+    public class ResponsiveYouTubeShortcode : SyncShortcode
+    {
+        private const string Id = nameof(Id);
+        private const string Title = nameof(Title);
+
+        public override ShortcodeResult Execute(
+            KeyValuePair<string, string>[] args,
+            string content,
+            IDocument document,
+            IExecutionContext context)
+        {
+            IMetadataDictionary arguments = args.ToDictionary(Id, Title);
+            arguments.RequireKeys(Id, Title);
+
+            XElement container = new XElement("div", new XAttribute("style", "position: relative;width: 100%;padding-bottom: 56.25%;margin: 1rem 0;"));
+
+            XElement iframe = new XElement(
+                "iframe",
+                new XAttribute("src", $"https://www.youtube.com/embed/{arguments.GetString(Id)}"),
+                new XAttribute("title", $"{arguments.GetString(Title)}"),
+                new XAttribute("style", "position: absolute;width: 100%;height: 100%;left: 0;top: 0;right: 0;"));
+
+            container.Add(iframe);
+
+            return container.ToString();
+        }
+    }
+}
