c41223d15177420a4d43fcb7eca64e818962c31e
Ankur Sheel <ankur.sheel@gmail.com>
2021-12-24 20:27:50 +1300

    refactor: add constants

--- src/StatiqHelpers/Modules/MetadataFromPath/GeneratePostDetailsFromPath.cs
+++ src/StatiqHelpers/Modules/MetadataFromPath/GeneratePostDetailsFromPath.cs
@@ -10,19 +10,25 @@ namespace StatiqHelpers.Modules.MetadataFromPath
 {
     public class GeneratePostDetailsFromPath : ParallelModule
     {
+        private const string Year = "year";
+        private const string Month = "month";
+        private const string Date = "date";
+        private const string Slug = "slug";
+
         protected override Task<IEnumerable<IDocument>> ExecuteInputAsync(IDocument input, IExecutionContext context)
         {
             context.LogDebug($"Read file {input.Source}");
 
             var postDetailsFromPath = input.GetPostDetailsFromPath();
-            var date = $"{postDetailsFromPath["year"].Value}-{postDetailsFromPath["month"].Value}-{postDetailsFromPath["date"].Value}";
+
+            var date = $"{postDetailsFromPath[Year].Value}-{postDetailsFromPath[Month].Value}-{postDetailsFromPath[Date].Value}";
 
             return Task.FromResult(
                 input.Clone(
                         new MetadataItems
                         {
                             { MetaDataKeys.PublishedDate, DateTime.ParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture) },
-                            { MetaDataKeys.Slug, postDetailsFromPath["slug"].Value }
+                            { MetaDataKeys.Slug, postDetailsFromPath[Slug].Value }
                         })
                     .Yield());
         }
