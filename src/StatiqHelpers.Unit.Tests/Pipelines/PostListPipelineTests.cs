993ec2f4eca6fdca753c27387be36b664488bb9d
Ankur Sheel <ankur.sheel@gmail.com>
2022-03-25 12:01:33 +1300

    test: add test for Post List Pipeline

---
+++ src/StatiqHelpers.Unit.Tests/Pipelines/PostListPipelineTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Testing;
+using StatiqHelpers.Pipelines;
+using VerifyXunit;
+using Xunit;
+
+namespace StatiqHelpers.Unit.Tests.Pipelines
+{
+    [UsesVerify]
+    public class PostListPipelineTests : BaseFixture
+    {
+        private readonly Bootstrapper _bootstrapper;
+        private const string PipelineName = nameof(PostListPipeline);
+
+        public PostListPipelineTests()
+        {
+            BaseSetUp();
+            _bootstrapper = PipelineTestHelpersStatic.GetBootstrapper();
+        }
+
+        [Fact]
+        public async Task Verify_dependencies()
+        {
+            await PipelineCommonTests.Verify_dependencies(_bootstrapper, PipelineName);
+        }
+
+        [Fact]
+        public async Task Verify_input_modules()
+        {
+            await PipelineCommonTests.Verify_input_modules(_bootstrapper, PipelineName);
+        }
+
+        [Fact]
+        public async Task Verify_process_modules()
+        {
+            await PipelineCommonTests.Verify_process_modules(_bootstrapper, PipelineName);
+        }
+
+        [Fact]
+        public async Task Verify_post_process_modules()
+        {
+            await PipelineCommonTests.Verify_post_process_modules(_bootstrapper, PipelineName);
+        }
+
+        [Fact]
+        public async Task Verify_output_modules()
+        {
+            await PipelineCommonTests.Verify_output_modules(_bootstrapper, PipelineName);
+        }
+
+        [Fact]
+        public async Task Sets_destination_to_blog()
+        {
+            var fileProvider = GetFileProvider();
+            var result = await _bootstrapper.RunTestAsync(fileProvider);
+
+            Assert.Equal((int)ExitCode.Normal, result.ExitCode);
+            var document = result.Outputs[PipelineName][Phase.Output].Single();
+            Assert.Equal("blog.html", document.Destination);
+        }
+
+        private TestFileProvider GetFileProvider()
+        {
+            var fileProvider = new TestFileProvider
+            {
+                "/input/Blog.cshtml",
+            };
+            return fileProvider;
+        }
+    }
+}
