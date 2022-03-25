940cbf3fc666b0e64ea3106116440141fb485ade
Ankur Sheel <ankur.sheel@gmail.com>
2022-03-25 17:20:33 +1300

    feat: add ReplaceImageLinks Module

---
+++ src/StatiqHelpers.Unit.Tests/Modules/ReplaceImageLinksTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading.Tasks;
+using Statiq.Common;
+using Statiq.Testing;
+using StatiqHelpers.Modules;
+using Xunit;
+
+namespace StatiqHelpers.Unit.Tests.Modules
+{
+    public class ReplaceImageLinksTests : BaseFixture
+    {
+        private readonly ReplaceImageLinks _module;
+
+        public ReplaceImageLinksTests()
+        {
+            _module = new ReplaceImageLinks("imagesDir");
+        }
+
+        [Fact]
+        public async Task Image_links_are_replaced_correctly_for_images_in_the_same_folder()
+        {
+            var content = "![Alt text 1](./image1.jpg)";
+
+            var document = GetTestDocument(content);
+
+            var result = await ExecuteAsync(document, _module).SingleAsync();
+
+            await AssertContents(result, "![Alt text 1](/imagesDir/slug/image1.jpg)");
+        }
+
+        [Fact]
+        public async Task Image_links_are_replaced_correctly_for_images_in_the_images_folder()
+        {
+            var content = "![Alt text 1](./images/image1.jpg)";
+
+            var document = GetTestDocument(content);
+
+            var result = await ExecuteAsync(document, _module).SingleAsync();
+
+            await AssertContents(result, "![Alt text 1](/imagesDir/slug/image1.jpg)");
+        }
+
+        [Fact]
+        public async Task Image_links_are_not_replaced_for_images_with_absolute_path()
+        {
+            var content = "![Alt text 1](/folder/image1.jpg)";
+
+            var document = GetTestDocument(content);
+
+            var result = await ExecuteAsync(document, _module).SingleAsync();
+
+            await AssertContents(result, "![Alt text 1](/folder/image1.jpg)");
+        }
+
+        private TestDocument GetTestDocument(string content)
+        {
+            var document = new TestDocument(new NormalizedPath("a.txt"), content)
+            {
+                { MetaDataKeys.Slug, "slug" }
+            };
+            return document;
+        }
+
+        private async Task AssertContents(TestDocument document, string expected)
+        {
+            var body = await document.GetContentStringAsync();
+            Assert.Equal(expected, body);
+        }
+    }
+}
