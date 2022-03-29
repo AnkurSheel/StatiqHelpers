4be399ddd4d24e897396cc4f8625972b96fc6f1c
Ankur Sheel <ankur.sheel@gmail.com>
2022-03-30 09:22:36 +1300

    :test: add some missing tests for Sitemap pipeline

--- src/StatiqHelpers.Unit.Tests/Pipelines/SitemapPipelineTests.cs
+++ src/StatiqHelpers.Unit.Tests/Pipelines/SitemapPipelineTests.cs
@@ -1,4 +1,8 @@
-﻿using StatiqHelpers.Pipelines;
+﻿using System.Xml;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Testing;
+using StatiqHelpers.Pipelines;
 
 namespace StatiqHelpers.Unit.Tests.Pipelines;
 
@@ -35,5 +39,44 @@ public class SitemapPipelineTests : PipelineBaseFixture
     public async Task Verify_output_modules()
     {
         await VerifyOutputModules(PipelineName);
+    }
+
+    [Fact]
+    public async Task Sitemap_is_created_for_all_pages_and_posts()
+    {
+        var fileProvider = GetFileProvider();
+        var result = await Bootstrapper.RunTestAsync(fileProvider);
+
+        Assert.Equal((int)ExitCode.Normal, result.ExitCode);
+        var document = result.Outputs[PipelineName][Phase.Output].Single();
+        var content = await document.GetContentStringAsync();
+
+        var doc = new XmlDocument();
+        doc.LoadXml(content);
+        await Verify(doc);
+    }
+
+    [Fact]
+    public async Task Sets_destination_to_sitemap_xml()
+    {
+        var fileProvider = GetFileProvider();
+        var result = await Bootstrapper.RunTestAsync(fileProvider);
+        var document = result.Outputs[PipelineName][Phase.Output].Single();
+
+        Assert.Equal((int)ExitCode.Normal, result.ExitCode);
+
+        Assert.Equal("sitemap.xml", document.Destination);
+    }
+
+    private TestFileProvider GetFileProvider()
+    {
+        var fileProvider = new TestFileProvider
+        {
+            "/input/Index.cshtml",
+            "/input/pages/markdownSlug/markdownFile.md",
+            "/input/pages/razorSlug/razorFile.cshtml",
+            "/input/posts/2022-03-27-slug/filename.md",
+        };
+        return fileProvider;
     }
 }
