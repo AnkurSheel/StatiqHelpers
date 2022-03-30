7d6c47dba375863b67fa2ca895f881297c072263
Ankur Sheel <ankur.sheel@gmail.com>
2022-03-31 08:57:13 +1300

    :test: add some missing tests for Rss pipeline

--- src/StatiqHelpers.Unit.Tests/Pipelines/RssPipelineTests.cs
+++ src/StatiqHelpers.Unit.Tests/Pipelines/RssPipelineTests.cs
@@ -1,4 +1,5 @@
-﻿using Statiq.App;
+﻿using System.Xml;
+using Statiq.App;
 using Statiq.Common;
 using Statiq.Testing;
 using StatiqHelpers.Pipelines;
@@ -45,9 +46,55 @@ namespace StatiqHelpers.Unit.Tests.Pipelines
         {
             var result = await Bootstrapper.RunTestAsync();
 
-            Assert.Equal((int) ExitCode.Normal, result.ExitCode);
+            Assert.Equal((int)ExitCode.Normal, result.ExitCode);
             var document = result.Outputs[PipelineName][Phase.Output].Single();
             Assert.Equal("rss.xml", document.Destination.ToString());
+        }
+
+        [Fact]
+        public async Task Rss_feed_is_created_for_posts()
+        {
+            var settings = new VerifyTests.VerifySettings();
+            var counter = 0;
+            settings.ScrubLinesWithReplace(
+                line =>
+                {
+                    if (counter == 0 && line.Contains("<pubDate>"))
+                    {
+                        counter++;
+                        return "<pubDate>pubdate</pubDate>";
+                    }
+
+                    return line;
+                });
+
+            settings.ScrubLinesWithReplace(
+                line => line.Contains("<lastBuildDate>")
+                    ? "<lastBuildDate>lastBuildDate</lastBuildDate>"
+                    : line);
+
+            var fileProvider = GetFileProvider();
+            var result = await Bootstrapper.RunTestAsync(fileProvider);
+
+            Assert.Equal((int)ExitCode.Normal, result.ExitCode);
+            var document = result.Outputs[PipelineName][Phase.Output].Single();
+            var content = await document.GetContentStringAsync();
+
+            var doc = new XmlDocument();
+            doc.LoadXml(content);
+            await Verify(doc, settings);
+        }
+
+        private TestFileProvider GetFileProvider()
+        {
+            var fileProvider = new TestFileProvider
+            {
+                "/input/Index.cshtml",
+                "/input/pages/markdownSlug/markdownFile.md",
+                "/input/posts/2022-02-27-slug1/filename.md",
+                "/input/posts/2022-03-27-slug2/filename.md",
+            };
+            return fileProvider;
         }
     }
 }
