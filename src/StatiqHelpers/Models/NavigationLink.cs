44575466a6fe8d364052e4cd0830187ce10e58d6
Ankur Sheel <ankur.sheel@gmail.com>
2022-04-25 23:33:36 +1200

    feat: add support for dropdown in nav links

--- src/StatiqHelpers/Models/NavigationLink.cs
+++ src/StatiqHelpers/Models/NavigationLink.cs
@@ -2,14 +2,23 @@
 {
     public class NavigationLink
     {
-        public NavigationLink(string title, string url)
+        public static NavigationLink NavigationLinkWithChildren(string title, IReadOnlyList<NavigationLink> childLinks)
+            => new NavigationLink(title, null, childLinks);
+
+        public static NavigationLink NavigationLinkWithoutChildren(string title, string url)
+            => new NavigationLink(title, url, null);
+
+        private NavigationLink(string title, string? url, IReadOnlyList<NavigationLink>? childLinks)
         {
             Title = title;
             Url = url;
+            ChildLinks = childLinks;
         }
 
         public string Title { get; }
 
-        public string Url { get; }
+        public string? Url { get; }
+
+        public IReadOnlyList<NavigationLink>? ChildLinks { get; }
     }
 }
