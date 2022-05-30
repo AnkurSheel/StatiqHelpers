43f8b0ec1582015147f4e0de8c41fd7662dab8ac
Ankur Sheel <ankur.sheel@gmail.com>
2022-05-31 10:35:36 +1200

    fix: optimize slug before replacing image links

--- src/StatiqHelpers.Unit.Tests/Modules/ReplaceImageLinksTests.cs
+++ src/StatiqHelpers.Unit.Tests/Modules/ReplaceImageLinksTests.cs
@@ -38,6 +38,18 @@ namespace StatiqHelpers.Unit.Tests.Modules
         }
 
         [Fact]
+        public async Task Path_is_optimized_and_filler_words_are_removed()
+        {
+            var content = "![Alt text 1](./image1.jpg)";
+
+            var document = GetTestDocument(content, "how to slug and new");
+
+            var result = await ExecuteAsync(document, _module).SingleAsync();
+
+            await AssertContents(result, "![Alt text 1](/imagesDir/slug-new/image1.jpg)");
+        }
+
+        [Fact]
         public async Task Image_links_are_not_replaced_for_images_with_absolute_path()
         {
             var content = "![Alt text 1](/folder/image1.jpg)";
@@ -49,11 +61,11 @@ namespace StatiqHelpers.Unit.Tests.Modules
             await AssertContents(result, "![Alt text 1](/folder/image1.jpg)");
         }
 
-        private TestDocument GetTestDocument(string content)
+        private TestDocument GetTestDocument(string content, string slug = "slug")
         {
             var document = new TestDocument(new NormalizedPath("a.txt"), content)
             {
-                { MetaDataKeys.Slug, "slug" }
+                { MetaDataKeys.Slug, slug }
             };
             return document;
         }
