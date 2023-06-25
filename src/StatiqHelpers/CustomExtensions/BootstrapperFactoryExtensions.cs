9c8776ce09d25d45df640ec6d42286f87b66cfb2
Ankur Sheel <ankur.sheel@gmail.com>
2023-06-25 15:04:57 +1200

    add to boostrapper

--- src/StatiqHelpers/CustomExtensions/BootstrapperFactoryExtensions.cs
+++ src/StatiqHelpers/CustomExtensions/BootstrapperFactoryExtensions.cs
@@ -1,11 +1,11 @@
+using StatiqHelpers.Analyzers;
 using StatiqHelpers.Commands;
 using StatiqHelpers.ImageHelpers;
 
-namespace StatiqHelpers.CustomExtensions
+namespace StatiqHelpers.CustomExtensions;
+
+public static class BootstrapperFactoryExtensions
 {
-    public static class BootstrapperFactoryExtensions
-    {
-        public static Bootstrapper CreateBootstrapper(this BootstrapperFactory factory, string[] args)
-            => factory.CreateWeb(args).RemovePipelines().AddCommand<ResizeImage>().AddCommand<NewPost>().AddPipelines().AddServices();
-    }
+    public static Bootstrapper CreateBootstrapper(this BootstrapperFactory factory, string[] args)
+        => factory.CreateWeb(args).RemovePipelines().AddCommand<ResizeImage>().AddCommand<NewPost>().AddPipelines().AddServices().AddAnalyzer<TitleLengthAnalyzer>().AddAnalyzer<ExcerptLengthAnalyzer>();
 }
