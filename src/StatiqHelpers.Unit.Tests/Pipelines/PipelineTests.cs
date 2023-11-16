894815ebf79ed55fef7405283744df6fe6d24b34
Ankur Sheel <ankur.sheel@gmail.com>
2023-11-17 00:15:42 +1300

    test: add new test to ensure that the correct pipelines are configured

---
+++ src/StatiqHelpers.Unit.Tests/Pipelines/PipelineTests.cs
@@ -0,0 +1,27 @@
+﻿using Statiq.Testing;
+
+namespace StatiqHelpers.Unit.Tests.Pipelines;
+
+[UsesVerify]
+public class PipelineTests : BaseFixture
+{
+    private readonly Bootstrapper _bootstrapper;
+
+    public PipelineTests()
+    {
+        _bootstrapper = PipelineTestHelpersStatic.GetBootstrapper().AddSetting(Keys.Host, "statiqhelpers.com");
+        BaseSetUp();
+    }
+
+    [Fact]
+    public async Task VerifyPipelines()
+    {
+        var result = await _bootstrapper.RunTestAsync(new TestFileProvider());
+
+        Assert.Equal((int)ExitCode.Normal, result.ExitCode);
+
+        var pipelineNames = result.Engine.Pipelines.Keys.ToList();
+        pipelineNames.Sort();
+        await Verify(pipelineNames);
+    }
+}
